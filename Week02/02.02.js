f4b553409daea4b48f843449f312d2064f945e5b
Erin Croteau <erin.croteau@gmail.com>
2020-08-20 18:14:31 -0400

    Added notes about how to commit  through Source Control icon in VS Code

--- Week02/02.02.js
+++ Week02/02.02.js
@@ -9,7 +9,7 @@ git status    //ok to enter this any time, tells you where you are and the statu
 2) git add .  //the . means all files in the current directory
 3) git commit -m "Commit message goes here"
 
-repeat 2 & 3 after every change in the file
+repeat 2 & 3 after every change in the file  (optional different way instead of command line/powershell: click on the Source Control icon on the very left border of the page - should have a number on it indicating uncommitted changes, then click on the plus sign just to the right of the file name, and then click on the checkmark above it and enter the commit message that you would've put between the quotation marks if you were doing git commit -m"dfjadfjladjf")
 
 If you wind up in the text editor for entering "git commit" without making a -m commit message, it will put you into the "VI" text editor to force you to make a commit message. To get out of this:
 Esc
