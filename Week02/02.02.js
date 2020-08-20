74b4faf051e3805ea88ee21d6de96bd817c0c106
Erin Croteau <erin.croteau@gmail.com>
2020-08-20 18:10:46 -0400

    3rd commit message

--- Week02/02.02.js
+++ Week02/02.02.js
@@ -1,3 +1,21 @@
-//Git initialization practice
+/*
+Git set up and practice
+
+For the folder with the files you want to make the git repository for: in VS Code, right-click on that folder and select Open in Integrated Terminal
+
+git status    //ok to enter this any time, tells you where you are and the status
+1) git init       //initializes empty Git repository in this folder
+
+2) git add .  //the . means all files in the current directory
+3) git commit -m "Commit message goes here"
+
+repeat 2 & 3 after every change in the file
+
+If you wind up in the text editor for entering "git commit" without making a -m commit message, it will put you into the "VI" text editor to force you to make a commit message. To get out of this:
+Esc
+:q!
+----------------------------------------------
+*/
+
 
 //This is a change
